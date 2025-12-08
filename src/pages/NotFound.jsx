eb73b7b280a25eb79a6ba6af2d59763c64589cdc
ariboss89 <ariboss89@gmail.com>
2025-12-08 09:10:47 +0700

    feat: adding not found page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="flex items-center justify-center">
+      <h1 className="text-red-500"> PAGE NOT FOUND </h1>
+    </div>
+  );
+}
+
+export default NotFound;
