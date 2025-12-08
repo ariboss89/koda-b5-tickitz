1527ed0ee3397f1d654d76d3ca8b305a74d22d3c
ariboss89 <ariboss89@gmail.com>
2025-12-08 09:11:31 +0700

    feat: create router for all the page

---
+++ src/Router.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Routes, Route } from "react-router";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import ViewAll from "./pages/ViewAll";
+import Register from "./pages/Register";
+
+function Router() {
+  /// => app
+  // Todolist => Todoist
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/Movies" element={<ViewAll />}></Route>
+      <Route path="/Login" element={<Login />}></Route>
+
+      <Route path="/Register" element={<Register />}></Route>
+      <Route path="*" element={<NotFound />}></Route>
+    </Routes>
+  );
+}
+
+export default Router;
