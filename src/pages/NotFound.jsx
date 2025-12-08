e3015791630699c087862e17723c0eb590716078
ariboss89 <ariboss89@gmail.com>
2025-12-08 10:37:42 +0700

    feat: adding header and footer

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+import NtFnd from "../assets/404.png";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 function NotFound() {
   return (
-    <div className="flex items-center justify-center">
-      <h1 className="text-red-500"> PAGE NOT FOUND </h1>
+    <div>
+      <Header />
+      <div className="flex items-center justify-center">
+        <img src={NtFnd} alt="not found" />
+      </div>
+      <Footer />
     </div>
   );
 }
