75ec62baa69e1b7b861a1bfbf46b1cc90701d9a8
ariboss89 <ariboss89@gmail.com>
2025-12-21 20:49:51 +0700

    feat: adding loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Loader() {
   return (
     <div>
-      <div class="flex flex-row items-center justify-center gap-2 bg-amber-300">
+      <div class="flex w-full flex-row items-center justify-center gap-2">
         <div class="h-4 w-4 animate-bounce rounded-full bg-blue-700"></div>
         <div class="h-4 w-4 animate-bounce rounded-full bg-blue-700 [animation-delay:-.3s]"></div>
         <div class="h-4 w-4 animate-bounce rounded-full bg-blue-700 [animation-delay:-.5s]"></div>
