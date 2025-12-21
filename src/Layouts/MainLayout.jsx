59903c8b8b414c9bf82941f56fc3011350ad9878
ariboss89 <ariboss89@gmail.com>
2025-12-21 20:51:28 +0700

    feat: adding layout

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -3,17 +3,18 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import FooterMobile from "../components/FooterMobile";
 import { Outlet } from "react-router";
-import useWindowWidth from "../hooks/useWindowWidth";
+// import useWindowWidth from "../hooks/useWindowWidth";
 
 function MainLayout() {
-  const width = useWindowWidth();
-  const breakpoint = 768;
+  // const width = useWindowWidth();
+  // const breakpoint = 768;
 
   return (
     <div>
       <Header />
       <Outlet />
-      {width > breakpoint ? <Footer /> : <FooterMobile />}
+      {/* {width > breakpoint ? <Footer /> : <FooterMobile />} */}
+      <Footer />
     </div>
   );
 }
